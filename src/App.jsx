9a73c787e8ebee1cfc67306f95637fdb91962989
Rashedalidesigner <rashedalidesigner@gmail.com>
2025-09-02 00:25:42 +0600

    fix router'

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Sidebar from './components/Sidebar/Sidebar'
-import {Route, Routes} from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Add from './pages/Add/Add';
 import Order from './pages/order/Order';
 import List from './pages/List/List';
@@ -9,19 +9,21 @@ import { ToastContainer } from 'react-toastify';
 
 function App() {
   return (
-    <div>
-      <ToastContainer/>
-      <Navbar/>
-      <hr />
-      <div className="app-content">
-        <Sidebar/>
-        <Routes>
-          <Route path="/add" element={<Add/>}/>
-          <Route path="/list" element={<List/>}/>
-          <Route path="/orders" element={<Order/>}/>
-        </Routes>
+    <BrowserRouter>
+      <div>
+        <ToastContainer />
+        <Navbar />
+        <hr />
+        <div className="app-content">
+          <Sidebar />
+          <Routes>
+            <Route path="/add" element={<Add />} />
+            <Route path="/list" element={<List />} />
+            <Route path="/orders" element={<Order />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   )
 }
 
